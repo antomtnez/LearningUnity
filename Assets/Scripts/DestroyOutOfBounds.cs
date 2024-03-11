a87e3d8bdeb57faf7efd210690623103524829e8
Anto Martínez <antomartinezdev@gmail.com>
2024-03-11 12:53:17 +0100

    Unit 2 - Create DestroyOutOfBounds component and added to animals and projectile prefabs

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30f;
+    private float lowerBound = -10f;
+
+    void Update()
+    {
+        if(transform.position.z > topBound || transform.position.z < lowerBound)
+            Destroy(gameObject);
+    }
+}
